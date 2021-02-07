988d704026ee1f534cf3cf53fc7a0d47e18bc139
Romain Froger <rom1.froger@hotmail.fr>
2021-02-07 20:13:34 +0100

    Linked GUI to youtube API

--- main.py
+++ main.py
@@ -1,12 +1,51 @@
 from pytube import YouTube
+import PyQt5.QtWidgets as qtw 
+import PyQt5.QtGui as qtg
 
-url = "https://www.youtube.com/watch?v=000al7ru3ms"
+class MainWindow(qtw.QWidget):
+    def __init__(self):
+        super().__init__()
+        #Set a title
+        self.setWindowTitle("YouTube Downloader")
 
-yt = YouTube(url)
-print(yt.title)
-versions = yt.streams.filter(progressive=True)
-for version in versions:
-    print(version.resolution)
+        #Set a layout
+        self.setLayout(qtw.QVBoxLayout())
+        #Create a label
+        my_label = qtw.QLabel("Welcome to free to use YouTube Downloader")
+        #Change the font size of label
+        my_label.setFont(qtg.QFont('Helvetica', 18))
+        self.layout().addWidget(my_label)
+        
+        #Set the url entry box
+        my_entry = qtw.QLineEdit()
+        my_entry.setObjectName("name_field")
+        my_entry.setText("Paste youtube url here")
+        self.layout().addWidget(my_entry)
+
+        #Create a button
+        my_button = qtw.QPushButton("Press me !",
+        clicked = lambda: press_it())
+        self.layout().addWidget(my_button)
+
+        self.show()
+
+        def press_it():
+            url = my_entry.text()
+            #clear entry
+            my_entry.setText('')
+            print(url)
+            yt = YouTube(url)
+            print(yt.title)
+            versions = yt.streams.filter(progressive=True)
+            for version in versions:
+                print(version.resolution)
+
+app = qtw.QApplication([])
+mw = MainWindow()
+
+#Run the app
+
+app.exec_()
 
 
-#video.download('./Downloads')
+
