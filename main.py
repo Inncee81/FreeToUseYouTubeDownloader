ef1e510ff2d080f1cd99eb0661dcd13331155652
Romain Froger <rom1.froger@hotmail.fr>
2021-02-07 19:23:34 +0100

    Returns available qualities for specifies videos

--- main.py
+++ main.py
@@ -1,7 +1,12 @@
-import pytube
+from pytube import YouTube
 
 url = "https://www.youtube.com/watch?v=000al7ru3ms"
-#Load url in youtube funnction
-youtube = pytube.YouTube(url)
-video = youtube.streams.get_highest_resolution()
-video.download('./Downloads')
+
+yt = YouTube(url)
+print(yt.title)
+versions = yt.streams.filter(progressive=True)
+for version in versions:
+    print(version.resolution)
+
+
+#video.download('./Downloads')
