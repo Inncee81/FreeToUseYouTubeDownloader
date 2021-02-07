47868d9008cb9cf4476a6d6967cee45cb034a2c5
Romain Froger <rom1.froger@hotmail.fr>
2021-02-07 20:58:30 +0100

    Added combo box with quality

--- main.py
+++ main.py
@@ -22,23 +22,43 @@ class MainWindow(qtw.QWidget):
         my_entry.setText("Paste youtube url here")
         self.layout().addWidget(my_entry)
 
+         #Create a combo box
+        my_combo = qtw.QComboBox(self)
+        my_combo.addItem("360p", "360p")
+        my_combo.addItem("480p", "480p")
+        my_combo.addItem("720p", "720p")
+        self.layout().addWidget(my_combo)
+
         #Create a button
-        my_button = qtw.QPushButton("Press me !",
+        my_button = qtw.QPushButton("Download",
         clicked = lambda: press_it())
         self.layout().addWidget(my_button)
+
+       
+        
 
         self.show()
 
         def press_it():
+
+            #Keep data
             url = my_entry.text()
+            quality = my_combo.currentText()
+
             #clear entry
             my_entry.setText('')
+
+            #Do pytube stuff
             print(url)
+            print(quality)
             yt = YouTube(url)
             print(yt.title)
+            #yt.streams.first().download('./Downloads')
+            """
             versions = yt.streams.filter(progressive=True)
             for version in versions:
                 print(version.resolution)
+            """
 
 app = qtw.QApplication([])
 mw = MainWindow()
