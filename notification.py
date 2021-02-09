26a2589c2a232eebb73b9ceed00a9e350a48fcc3
Romain Froger <rom1.froger@hotmail.fr>
2021-02-09 01:52:36 +0100

    Added notification file

---
+++ notification.py
@@ -0,0 +1,7 @@
+import notify2, os
+
+def notif_download_started():
+    notify2.init("Starting Download")
+    n = notify2.Notification(summary="Download started",message="Wait for a few seconds while file is being downloaded")
+    n.set_urgency(notify2.URGENCY_NORMAL)
+    n.show() 
